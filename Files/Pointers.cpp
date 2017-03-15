fd7d3f262c01ecdc4b87897dc3309d3b26159c15
Ninosauleo <ninosauleo@gmail.com>
2017-03-15 20:10:33 +0100

    Pointers

---
+++ Files/Pointers.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+/* POINTERS ARE ordinary variables that can store addresses of variables
+ *
+ * type * is used to create a variable that stores an address
+ *
+ * *variable_name is to get the value from the address
+ */
+int main()
+{
+    int var = 5;
+    int a = 20;
+
+    cout << *&var << endl;
+
+    int * const p = &var; //that asterisk here is just used to INFORM about that this variable is a POINTER so it is a variable that can point to address of another variable
+
+    // p = &var;
+
+    cout << *p << endl; //that asterisk here is used to RETRIEVE (GET) value FROM indicated (pointed) area in our memory (address)
+
+    //*p = 20;
+//    var = 60;
+    *p = 60;
+    cout << "var: " << var << endl;
+    cout << "*p: "<< *p << endl;
+    cout << "a: "<< a << endl;
+
+    int * const p_const = &a; //this is a pointer that has to be initialized when defined, because it cannot change after defining the thing that it is pointing to (address)
+
+    const int * p_2 = &a; //this is a pointer that cannot change the value that is under address its pointing to.
+
+    const int * const p_3 = &a; //this is a pointer that cannot change the value that is under address its pointing to and also it cant change the address
+
+    cout << endl << endl << endl;
+
+    int ordinary_var = 10;
+
+    int *ordinary_p = &ordinary_var;
+
+    cout << "ordinary_var: " << ordinary_var << endl; //integer value
+    cout << "&ordinary_var: " << &ordinary_var << endl; //integer value
+    cout << "ordinary_p: " << ordinary_p << endl; //address
+    cout << "*ordinary_p: " << *ordinary_p << endl; //integer value from pointed place (ordinary_var)
+    cout << "&ordinary_p: " << &ordinary_p << endl; //address of pointer itself
+
+    int ** p_pointing_to_address_of_pointer = &ordinary_p;
+
+    cout << "p_pointing_to_address_of_pointer: " << p_pointing_to_address_of_pointer << endl;
+
+    int b[3];
+    b[0] = 0;
+    b[1] = 20;
+    b[2] = 40;
+    short int zm;
+    int * const p_1 = &b[0];
+
+    cout << p_1 << endl;
+    cout << a << endl;
+
+    cout << endl << endl << endl;
+
+    cout << &b[0] << endl;
+    cout << &b[1] << endl;
+    cout << &b[2] << endl;
+
+    cout << endl << endl << endl;
+
+    cout << b << endl;
+    cout << b + 1 << endl;
+    cout << b + 2 << endl;
+
+    cout << endl << endl << endl;
+
+    cout << *b << endl;
+    cout << *(b + 1) << endl;
+    cout << *(b + 2) << endl;
+
+    cout << endl;
+
+    cout << b[0] << endl;
+    cout << b[1] << endl;
+    cout << b[2] << endl;
+
+    // cout << a++ << endl; // a++  a = a + 1
+
+    cout << endl;
+
+    int * p3 = &b[0];
+
+    cout << *p3 << endl;  // 0
+    cout << ++*p3 << endl; //1
+    cout << *++p3 << endl; //20
+    cout << *p3++ << endl; //20
+    cout << *p3 << endl; //40
+
+    return 0;
+}
